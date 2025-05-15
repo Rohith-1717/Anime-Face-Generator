51e3fe8c2036dfc71ded096abc4cede65a1594de
Rohith-1717 <dm.rohithp@gmail.com>
2025-05-15 19:16:30 +0530

    Changes xd

--- Streamlit.py
+++ Streamlit.py
@@ -16,22 +16,39 @@ st.markdown(
     f"""
     <style>
     .stApp {{background-image: url("data:image/jpg;base64,{bg_image_base64}"); background-size: cover;
-        background-position: center; background-repeat: no-repeat;}}
+        background-position: center; background-repeat: no-repeat; }}
 
-    .custom-title {{background-color: #ff69b4; color: white; font-size: 2.5em; text-align: center; padding: 8px 16px;
-        border-radius: 12px; display: inline-block; margin-bottom: 25px; }}
+    .custom-title {{ background: linear-gradient(135deg, #ff66cc, #ff1493, #ffc0cb);
+        color: white; font-size: 2.5em; text-align: center; padding: 8px 16px; border-radius: 12px;
+        display: inline-block; margin-bottom: 25px; font-weight: bold;
+        box-shadow: 0 0 10px rgba(255, 105, 180, 0.5);}}
+    .custom-title-container {{ text-align: center; }}
 
-    .custom-title-container {{text-align: center; }}
+    .custom-label {{ background: linear-gradient(135deg, #ff66cc, #ff1493, #ffc0cb); color: white;
+        font-size: 1em; padding: 8px 14px; border-radius: 10px; display: inline-block; margin-bottom: 10px; font-weight: bold;
+        box-shadow: 0 0 6px rgba(255, 105, 180, 0.4); }}
 
-    .custom-label {{ background-color: #ff69b4; color: white;font-size: 1em; padding: 8px 14px; border-radius: 10px; display: inline-block; margin-bottom: 10px; }}
+    .stButton>button {{display: block; margin: auto; background-color: #FF69B4;
+        color: white;  border: none; padding: 0.75em 1.5em; border-radius: 12px;
+        font-size: 1.1em; font-weight: bold;  box-shadow: 2px 2px 6px rgba(0,0,0,0.3);
+        position: relative; overflow: hidden; transition: 0.3s ease-in-out; }}
 
-    .stButton>button {{ display: block; margin: auto; background-color: #FF69B4;
-        color: white; border: none;
-        padding: 0.75em 1.5em; border-radius: 12px;font-size: 1.1em; font-weight: bold; box-shadow: 2px 2px 6px rgba(0,0,0,0.3); transition: 0.3s;}}
+    .stButton>button::before {{content: '';
+        position: absolute; top: 0;
+        left: -75%; width: 50%;
+        height: 100%;
+        background: linear-gradient(120deg, rgba(255,255,255,0.2) 0%, rgba(255,255,255,0.5) 50%, rgba(255,255,255,0.2) 100%);
+        transform: skewX(-20deg); }}
 
-    .stButton>button:hover {{ background-color: #ff85c1; transform: scale(1.05); }}
+    .stButton>button:hover::before {{ left: 130%;
+        transition: left 0.7s ease-in-out; }}
 
-    .stNumberInput>div>input {{  background-color: #2f3136; color: white; border-radius: 8px;  border: none; padding: 10px; }}
+    .stButton>button:hover {{color: white !important;}}
+
+
+    .stNumberInput>div>input {{ background-color: #2f3136;
+        color: white; border-radius: 8px;
+        border: none; padding: 10px; }}
     </style>
     """,
     unsafe_allow_html=True
@@ -48,7 +65,6 @@ gen = lg()
 st.markdown('<div class="custom-title-container"><div class="custom-title">Anime Face Generator</div></div>', unsafe_allow_html=True)
 st.markdown('<div class="custom-label">Enter the number of faces you want to generate and click the button below:</div>', unsafe_allow_html=True)
 n_imgs = st.number_input("", min_value=1, max_value=500, value=1, step=1)
-
 
 if st.button("Generate"):
     noise = torch.randn(n_imgs, nz, 1, 1)
